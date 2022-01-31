5758ee40f3ee3c041429e87e4e5b3767d20d922b
Egor Samoylin <e.samoylin@gmail.com>
2022-02-01 01:04:09 +0300

    stage 4

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+const personName = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+};
+
+export default personName;
