41ab5f469cbb76a5669564635398843602889a2b
Egor Samoylin <e.samoylin@gmail.com>
2022-02-26 22:49:39 +0300

    fix brain-even.js

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,54 +1,29 @@
-#!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import inquireName, { userName } from '../../src/cli.js'; // импорт имени пользователя из cli.js
+import startGame, { rounds } from '../index.js';
+import generateRandomNumber from '../utils.js';
 
-let result = 'Correct';
+const gameInstruction = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const instructionGame = () => {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-};
-
-const congratulations = () => {
-  console.log(`Congratulations, ${userName}!`);
-};
-
-const evenNumbers = () => {
-  const maxRandomNumber = 20;
-  const avoidZero = 1; // избежать 0 при рандоме
-  const randomNumber = Math.floor(Math.random() * maxRandomNumber) + avoidZero;
-  console.log(`Question: ${randomNumber}`);
-  const answer = readlineSync.question('Your answer: ');
-
-  if (randomNumber % 2 === 0 && answer === 'yes') {
-    console.log('Correct!');
-    result = 'Correct';
-  } else if (randomNumber % 2 === 1 && answer === 'no') {
-    console.log('Correct!');
-    result = 'Correct';
-  } else {
-    if (randomNumber % 2 === 0) {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.`);
-      console.log(`Let's try again, ${userName}!`);
-      result = 'Wrong';
-    }
-    if (randomNumber % 2 === 1) {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.`);
-      console.log(`Let's try again, ${userName}!`);
-      result = 'Wrong';
-    }
+const calculate = (num) => {
+  if (num % 2 === 0) {
+    return 'yes';
   }
+  return 'no';
 };
 
-console.log('Welcome to the Brain Games!');
-inquireName(); /* Приветствие пользователя */
-instructionGame(); /* Инструкиця к игре */
+const generateRound = () => {
+  const num = generateRandomNumber(1, 10);
+  const question = `Question: ${num}`;
+  const result = calculate(num);
 
-for (let i = 0; i < 3; i += 1) {
-  if (result === 'Correct') {
-    evenNumbers();
+  return [question, result];
+};
+
+const generateRounds = () => {
+  const gameRounds = [];
+  for (let i = 0; i < rounds; i += 1) {
+    gameRounds.push(generateRound());
   }
-}
+  return gameRounds;
+};
 
-if (result === 'Correct') {
-  congratulations();
-}
+startGame(gameInstruction, generateRounds());
